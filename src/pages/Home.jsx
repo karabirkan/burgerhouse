ada8ad9e02a5f937709eebaab1dfe0390238a653
tokyoparis <birkannewyork@gmail.com>
2023-08-04 08:53:55 +0300

    logic for slide's buttons

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -19,6 +19,20 @@ export default function Home() {
     },
   ];
 
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const prevSlide = () => {
+    const isFirstSlide = currentIndex === 0;
+    const newIndex = isFirstSlide ? slides.length - 1 : currentIndex - 1;
+    setCurrentIndex(newIndex);
+  };
+
+  const nextSlide = () => {
+    const isLastSlide = slides.length - 1;
+    const newIndex = isLastSlide ? 0 : currentIndex + 1;
+    setCurrentIndex(newIndex);
+  };
+
   return (
     <Layout>
       <div className="max-w-[1400px] h-[780px] w-full m-auto py-16 px-4 relative group">
