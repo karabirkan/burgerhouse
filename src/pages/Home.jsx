330e159eb1d6f022c546495ebffe158c3d2828c6
tokyoparis <birkannewyork@gmail.com>
2023-08-04 12:14:22 +0300

    slide improved : button's centered, images centered

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,10 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import Layout from "../components/Layout/Layout";
 import slide2Image from "../images/slide_2.jpg";
 import slide3Image from "../images/slide_3.jpg";
 import slide4Image from "../images/slide_4.jpg";
+import slide5Image from "../images/slide_5.jpg";
+import slide6Image from "../images/slide_6.jpg";
 import { BsChevronCompactLeft } from "react-icons/bs";
 import { BsChevronCompactRight } from "react-icons/bs";
 
@@ -17,33 +19,39 @@ export default function Home() {
     {
       url: slide4Image,
     },
+    {
+      url: slide5Image,
+    },
+    {
+      url: slide6Image,
+    },
   ];
 
   const [currentIndex, setCurrentIndex] = useState(0);
 
   const prevSlide = () => {
-    const isFirstSlide = currentIndex === 0;
-    const newIndex = isFirstSlide ? slides.length - 1 : currentIndex - 1;
+    const newIndex = (currentIndex - 1 + slides.length) % slides.length;
     setCurrentIndex(newIndex);
   };
 
   const nextSlide = () => {
-    const isLastSlide = slides.length - 1;
-    const newIndex = isLastSlide ? 0 : currentIndex + 1;
+    const newIndex = (currentIndex + 1) % slides.length;
     setCurrentIndex(newIndex);
   };
 
   return (
     <Layout>
       <div className="max-w-[1400px] h-[780px] w-full m-auto py-16 px-4 relative group">
-        <div
-          className="w-full h-full rounded-2xl bg-center bg-cover duration-500"
-          style={{ backgroundImage: `url(${slides[currentIndex].url})` }}
-        ></div>
-        <div className="hidden group-hover:block absolute top-[50%] -translate-x-0 -translate-y-[-50%] left-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer">
+        <img
+          className=" rounded-2xl object-cover duration-500"
+          src={slides[currentIndex].url}
+          alt=""
+        />
+
+        <div className="group-hover:block absolute top-1/3 left-5 transform -translate-y-1/2 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer">
           <BsChevronCompactLeft onClick={prevSlide} size={30} />
         </div>
-        <div className="hidden group-hover:block absolute top-[50%] -translate-x-0 -translate-y-[-50%] right-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer">
+        <div className="group-hover:block absolute top-1/3 right-5 transform -translate-y-1/2 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer">
           <BsChevronCompactRight onClick={nextSlide} size={30} />
         </div>
       </div>
